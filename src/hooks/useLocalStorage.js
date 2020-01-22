909a08d184647ffd276b22422789635be53f766a
Michael Padiernos <mike.padiernos@gmail.com>
2020-01-21 22:45:42 -0500

    FEATURE:(Dark mode): Implemented dark mode
    - added hook useDarkMode to NavBar

--- src/hooks/useLocalStorage.js
+++ src/hooks/useLocalStorage.js
@@ -1,12 +1,18 @@
 import { useState } from "react";
 
 export const useLocalStorage = (key, initialValue) => {
+
+	// const [storedValue, setStoredValue] = useState(() => {
+	// 	if (window.localStorage.getItem(key)) {
+	// 		return JSON.parse(window.localStorage.getItem(key));
+	// 	}
+	// 	window.localStorage.setItem(key, JSON.stringify(initialValue));
+	// 	return initialValue;
+	// });
+
 	const [storedValue, setStoredValue] = useState(() => {
-		if (window.localStorage.getItem(key)) {
-			return JSON.parse(window.localStorage.getItem(key));
-		}
-		window.localStorage.setItem(key, JSON.stringify(initialValue));
-		return initialValue;
+		const item = window.localStorage.getItem(key);
+		return item ? JSON.parse(item) : initialValue;
 	});
 
 	const setValue = value => {
@@ -15,4 +21,5 @@ export const useLocalStorage = (key, initialValue) => {
 	};
 
 	return [storedValue, setValue];
+
 };
